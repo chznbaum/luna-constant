24a5271ab56efbd255ecc69b1e82912488b1f683
Chazona Baum <chznbaum@gmail.com>
2017-07-29 10:40:43 -0400

    Implemented static page

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,14 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @mom_name = "Tatiana Constant"
+    @child_name = "Luna Constant"
+    @site_title = "#{@child_name}'s Photo Site"
+    @page_title = ""
+  end
+end
