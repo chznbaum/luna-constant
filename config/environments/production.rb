57777812aff1a4e748a4ae8289cc688b2f7e2fbf
Chazona Baum <chznbaum@gmail.com>
2017-08-01 00:08:20 -0400

    Set up SendGrid and custom url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "private-photos_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { host: 'lunaconstant.com' }
+  config.action_mailer.default_url_options = { host: 'http://lunaconstant.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
