48b1a56c630719164ef68ab5dabeda617fd6eb53
Chazona Baum <chznbaum@gmail.com>
2017-08-13 21:47:34 -0400

    Set number of users per page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   access all: [], user: [], helper: :all, site_admin: :all
 
   def index
-    @users = User.all.order("created_at ASC").page(params[:page]).per(20)
+    @users = User.all.order("created_at ASC").page(params[:page]).per(12)
   end
 
 end
